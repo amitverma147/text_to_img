f4a2e1cc761dec96dbd89a64ffdcd0d73d04d1fe
Amit Verma <vamit20000@gmail.com>
2025-07-04 23:27:59 +0530

    plans done

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -1,5 +1,30 @@
+import { assets, plans } from "../assets/assets";
+
 const BuyCredit = () => {
-  return <div>BuyCredit</div>;
+  return (
+    <div className="min-h-[80vh] text-center pt-14 mb-10">
+      <button className="border border-gray-400 px-10 py-2 rounded-full mb-6">
+        Our Plans
+      </button>
+      <h1 className="text-center text-3xl font-medium mb-6 sm:mb-10">
+        Choose the plan
+      </h1>
+      <div>
+        {plans.map((item, index) => {
+          return (
+            <div key={index}>
+              <img src={assets.lock_icon} alt="price-logo" />
+              <p>{item.id}</p>
+              <p>{item.desc}</p>
+              <p>
+                {item.price} / {item.credits} credits
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default BuyCredit;
