ac583c9f94a23c677d3be2f421e1a6c32bd610dc
Amit Verma <vamit20000@gmail.com>
2025-07-05 00:55:50 +0530

    motion addition left

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import Navbar from "./components/Navbar";
 import BuyCredit from "./pages/BuyCredit";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
-import Footer from './components/Footer'
+import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import { useContext } from "react";
+import { AppContext } from "./context/AppContext";
 const App = () => {
+  const { showLogin } = useContext(AppContext);
   return (
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
-      <Navbar/>
+      <Navbar />
+      {showLogin && <Login />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
         <Route path="/buycredit" element={<BuyCredit />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const { user } = useContext(AppContext);
+  const { user,setShowLogin } = useContext(AppContext);
 
   return (
     <div className="flex justify-between items-center py-4">
@@ -44,7 +44,7 @@ const Navbar = () => {
             >
               Pricing
             </p>
-            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
+            <button onClick={()=> setShowLogin(true)} className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
               Login
             </button>
           </div>
